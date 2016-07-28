756441ce095feae5e6f3f532aaefe7fcaea9cf04
blabladuck <contactveej@gmail.com>
2016-07-28 14:42:43 +0530

    Revert "set text"
    This reverts commit 5fc02f3a3c966948d6c65c30ef003fe64b847b67.

--- app/src/main/java/com/example/sanjeev/hellogit/MainActivity.java
+++ app/src/main/java/com/example/sanjeev/hellogit/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.sanjeev.hellogit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txt = (TextView)findViewById(R.id.txt);
-        txt.setText("Text");
+
     }
 }
