5fc02f3a3c966948d6c65c30ef003fe64b847b67
blabladuck <contactveej@gmail.com>
2016-07-28 14:35:33 +0530

    set text

--- app/src/main/java/com/example/sanjeev/hellogit/MainActivity.java
+++ app/src/main/java/com/example/sanjeev/hellogit/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.sanjeev.hellogit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView txt = (TextView)findViewById(R.id.txt);
+        txt.setText("Text");
     }
 }
